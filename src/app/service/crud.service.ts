0ef2c2ac6796e8ac821d7e04e1b7a9e7aa189d12
Gian <gianpolastri@gmial.com>
2023-09-15 12:35:05 -0300

    feat(src/app/service): completed crud for tasks

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from 'src/app/model/task'
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,18 @@ export class CrudService {
 
   constructor( private http : HttpClient) {
     this.serviceURL = "http://localhost:300/tasks"
+   }
+
+   addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.serviceURL, task);
+   }
+   getAllTask(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.serviceURL);
+   }
+   deleteTask(task: Task): Observable<Task> {
+    return this.http.delete<Task>(this.serviceURL+'/'+task.id);
+   }
+   editTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(this.serviceURL+'/'+task.id, task);
    }
 }
